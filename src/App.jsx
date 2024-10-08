2d8716f2ea1247750a9b6fbc17a6919d44eb866c
tiago-edu <tiagomoreiraeduardo@gmail.com>
2024-10-08 18:35:48 -0300

    new feat IA

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,28 @@ import Login from './pages/Login'
 import Us from './pages/Us'
 import Perfil from './pages/Perfil'
 import Contact from './pages/Contact'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/login" element={<Login />} /> 
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<Us />} />
-          <Route exact path="/perfil" element={<Perfil />} />
-          <Route exact path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} /> 
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<Us />} />
+            <Route exact path="/perfil" element={<Perfil />} />
+            <Route exact path="/contact" element={<Contact />} />
+
+          </Routes>
+        </Router>
       </div>
-      </>
+    </>
   )
 }
 
-export default App
-{/*Botar Id */}
+export default App;
