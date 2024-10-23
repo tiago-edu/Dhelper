8b2b3252f2ee0d6b6e6e4f0a0a02ec620d0d1ca9
tiago-edu <tiagomoreiraeduardo@gmail.com>
2024-10-23 14:40:11 -0300

    add router.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import './App.css'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Us from './pages/Us'
-import Perfil from './pages/Perfil'
-import Contact from './pages/Contact'
-import Home from './pages/Home';
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+// App.jsx
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRouter from './routes/router.jsx'; 
 function App() {
   return (
-    <>
-      <div className="app">
-        <Router>
-          <Routes>
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/login" element={<Login />} /> 
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/about" element={<Us />} />
-            <Route exact path="/perfil" element={<Perfil />} />
-            <Route exact path="/contact" element={<Contact />} />
-
-          </Routes>
-        </Router>
-      </div>
-    </>
-  )
+    <div className="app">
+      <Router>
+        <AppRouter />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/routes/router.jsx
@@ -0,0 +1,24 @@
+// router.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Register from '../pages/Register';
+import Login from '../pages/Login';
+import Us from '../pages/Us';
+import Perfil from '../pages/Perfil';
+import Contact from '../pages/Contact';
+import Home from '../pages/Home';
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<Us />} />
+      <Route exact path="/perfil" element={<Perfil />} />
+      <Route exact path="/contact" element={<Contact />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
