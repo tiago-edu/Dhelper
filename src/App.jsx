a3c984801b682aea9db2d9368f23ebcbd2319c08
tiago-edu <tiagomoreiraeduardo@gmail.com>
2024-11-26 19:40:43 -0300

    fix dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './routes/router.jsx'; 
-import { styles } from './utils/styles.jsx';
-import FileInput from './components/FileInput.tsx';
+import AppRouter from './routes/router.jsx';
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+
 function App() {
-  // const onSubmit = () => {};
   return (
     <Provider store={store}>
       <div className="app">
@@ -15,16 +13,6 @@ function App() {
         </Router>
       </div>
     </Provider>
-
-    <div className="app">
-      <Router>
-        <AppRouter />
-      </Router>
-      {/* <form onSubmit={onSubmit} className={`${styles.formSubmit}`}>
-        <FileInput />
-      </form> */}
-    </div>
-
   );
 }
 
